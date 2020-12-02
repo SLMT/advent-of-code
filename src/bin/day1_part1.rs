e5557fe99f95df8e7e01dc941f2970ce08c2ba8d
SLMT <sam123456777@gmail.com>
2020-12-02 11:47:09 +0800

    Finish day1

---
+++ src/bin/day1_part1.rs
@@ -0,0 +1,47 @@
+
+use std::io;
+
+fn main() {
+  let mut input = String::new();
+  let mut numbers = vec![];
+
+  loop {
+    // Read inputs
+    input.clear();
+    let read_count = io::stdin().read_line(&mut input).unwrap();
+    if read_count == 0 { // EOF
+      break;
+    }
+
+    // Parse the number
+    let number: i32 = input.trim().parse().unwrap();
+    numbers.push(number);
+  }
+
+  println!("{}", solve(&numbers));
+}
+
+fn solve(numbers: &Vec<i32>) -> i32 {
+  for number_1 in numbers {
+    // Find another number
+    let number_2 = 2020 - number_1;
+
+    // Check existence
+    if numbers.contains(&number_2) {
+      return number_1 * number_2;
+    }
+  }
+
+  panic!("there is no answer!");
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test1() {
+    let numbers = vec![1721, 979, 366, 299, 675, 1456];
+    assert_eq!(solve(&numbers), 514579);
+  }
+}
